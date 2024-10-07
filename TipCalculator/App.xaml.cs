99e2b59736cd128b2186ec599767b58e10b0672e
tobias-roy <tobiasroy94@gmail.com>
2024-10-07 22:47:39 +0200

    TipCalculatorV1

---
+++ TipCalculator/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace TipCalculator;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("da-DK");
+        CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("da-DK");
+
+        MainPage = new AppShell();
+    }
+}
