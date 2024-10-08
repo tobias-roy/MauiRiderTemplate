36ff56fc8f1bea7e8b3b986a547b8c3576bafd5e
tobias-roy <tobiasroy94@gmail.com>
2024-10-08 14:52:50 +0200

    Flyout menu created

--- TipCalculator/Controls/FlyoutHeader.xaml.cs
+++ TipCalculator/Controls/FlyoutHeader.xaml.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TipCalculator.Views;
+namespace TipCalculator.Controls;
 
-public partial class GridCellTest : ContentPage
+public partial class FlyoutHeader : ContentView
 {
-    public GridCellTest()
+    public FlyoutHeader()
     {
         InitializeComponent();
     }
