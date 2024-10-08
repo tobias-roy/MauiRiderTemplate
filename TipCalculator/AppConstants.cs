b725adacb885e8152c85a98dee4713a447c44895
tobias-roy <tobiasroy94@gmail.com>
2024-10-08 12:34:21 +0200

    Calculator grid made with static color assets

---
+++ TipCalculator/AppConstants.cs
@@ -0,0 +1,10 @@
+namespace AppConstants;
+
+public class StylingConstants
+{
+    public static Color GreyBtnColor = Color.FromRgb(169,169,169);
+    public static Color OrangeBtnColor = Color.FromRgb(255,128,18);
+    public static Color WhiteTextColor = Color.FromRgb(255,255,255);
+    public static Color DarkGreyTextColor = Color.FromRgb(64,64,64);
+    public static double BtnFontSize = 30;
+}
